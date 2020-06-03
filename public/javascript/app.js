11e44923706f04add6f31123d1c66b3b3807379b
Carlos Gasperi <cgasperi90@gmail.com>
2020-06-03 18:29:48 -0400

    making the site run on express, changed the color scheme, and changed the way the projects are displayed on site.

---
+++ public/javascript/app.js
@@ -0,0 +1,115 @@
+//here we will make our on click function to enter the website.
+// $("#enter").on("click", function() {
+    
+//     //here we are giving the front page jumbotron an animation
+//     //when the enter button is clicked.
+//     $("#front-page").addClass("animated bounceOutUp");
+    
+//     //here we create a new div for the projects to go into.
+//     //we also give this div an id and some classes.
+//     //$("#second-page").empty();
+
+    
+//     var projectsPage = $("<div>");
+//     projectsPage.attr("id", "project-page");
+//     projectsPage.addClass("jumbotron animated bounceInUp delay-1s");
+//     $("#second-page").append(projectsPage);
+
+//     var welcomeHead = $("<h1>");
+//     welcomeHead.html("My Projects!<hr class='my-4'>");
+//     $("#project-page").append(welcomeHead);
+
+//     //here we will create more divs for the content of the page.
+//     //Image 1
+//     var image = $("<img>");
+//     image.addClass("img-thumbnail rounded");
+//     image.attr("src", "./assets/images/Game Bros Project.png");
+//     $("#project-page").append(image);
+    
+//     //Button 1
+//     var button = $("<a>");
+//     button.attr("id", "picButtons");
+//     button.attr("href", "https://mikevong.github.io/Project-1/");
+//     button.attr("target", "_blank");
+//     button.addClass("btn btn-primary");
+//     button.text("GitHub");
+//     $("#project-page").append(button);
+    
+//     //Image 2
+//     var image2 = $("<img>");
+//     image2.addClass("img-thumbnail rounded");
+//     image2.attr("src", "./assets/images/About Me Page.png");
+//     $("#project-page").append(image2);
+
+//     //Button 2
+//     var button2 = $("<a>");
+//     button2.attr("id", "picButtons");
+//     button2.attr("href", "https://cgasperi90.github.io/Bootstrap-Portfolio/");
+//     button2.attr("target", "_blank");
+//     button2.addClass("btn btn-primary");
+//     button2.text("GitHub");
+//     $("#project-page").append(button2);
+
+//     //Image 3
+//     var image3 = $("<img>");
+//     image3.addClass("img-thumbnail rounded");
+//     image3.attr("src", "./assets/images/Giphy App.png");
+//     $("#project-page").append(image3);
+
+//     //Button
+//     var button3 = $("<a>");
+//     button3.attr("id", "picButtons");
+//     button3.attr("href", "https://cgasperi90.github.io/GiphyApp/");
+//     button3.attr("target", "_blank");
+//     button3.addClass("btn btn-primary");
+//     button3.text("GitHub");
+//     $("#project-page").append(button3);
+
+//     //Image 4
+//     var image4 = $("<img>");
+//     image4.addClass("img-thumbnail rounded");
+//     image4.attr("src", "./assets/images/NFL Trivia.png");
+//     $("#project-page").append(image4);
+
+//     //Button
+//     var button4 = $("<a>");
+//     button4.attr("id", "picButtons");
+//     button4.attr("href", "https://cgasperi90.github.io/TriviaGame/");
+//     button4.attr("target", "_blank");
+//     button4.addClass("btn btn-primary");
+//     button4.text("GitHub");
+//     $("#project-page").append(button4);
+
+//     //Image 5
+//     var image5 = $("<img>");
+//     image5.addClass("img-thumbnail rounded");
+//     image5.attr("src", "./assets/images/Psychic Game.png");
+//     $("#project-page").append(image5);
+
+//     //Button
+//     var button5 = $("<a>");
+//     button5.attr("id", "picButtons");
+//     button5.attr("href", "https://cgasperi90.github.io/Psychic-Game/");
+//     button5.attr("target", "_blank");
+//     button5.addClass("btn btn-primary");
+//     button5.text("GitHub");
+//     $("#project-page").append(button5);
+
+//     //Image 6
+//     var image6 = $("<img>");
+//     image6.addClass("img-thumbnail rounded");
+//     image6.attr("src", "./assets/images/Train Time App.png");
+//     $("#project-page").append(image6);
+
+//     //Button
+//     var button6 = $("<a>");
+//     button6.attr("id", "picButtons");
+//     button6.attr("href", "https://cgasperi90.github.io/TrainTime/");
+//     button6.attr("target", "_blank");
+//     button6.addClass("btn btn-primary");
+//     button6.text("GitHub");
+//     $("#project-page").append(button6);
+
+
+
+// });
