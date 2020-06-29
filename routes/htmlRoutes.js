aa69cae633b6749175b3d7d2cdc678c3aa04f1fc
Carlos Gasperi <cgasperi90@gmail.com>
2020-06-29 18:34:44 -0400

    adding a contact page to display phone number and email address.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,4 +6,8 @@ module.exports = function(app) {
     app.get("/projects", function(req, res) {
         res.render("projects")
     })
+
+    app.get("/contact", function(req, res) {
+        res.render("contact");
+    })
 };
